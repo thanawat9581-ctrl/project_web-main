7ecf3a86176d35346372e7213958b5d0da2e5559
Affan888 <thnawat9581@gmail.com>
2026-02-27 16:23:53 +0700

    asdad

--- templates/footer.php
+++ templates/footer.php
@@ -2,4 +2,5 @@
         <p style="text-align: center; color: #888;">
             &copy; <?= date('Y') ?>. จัดทำโดย Affan, Peter และ 999.
         </p>
+        <p>asdasdasdadasdasda</p>
     </footer>
