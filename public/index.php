deafdffa6a01cd52182807eaf6bb4b5ed0fdab1d
Affan888 <thnawat9581@gmail.com>
2026-03-14 01:13:02 +0700

    Co-authored-by: ! 999 <Jesusbro909@users.noreply.github.com> Co-authored-by: Phongsathad <Phongsathad@users.noreply.github.com>

--- public/index.php
+++ public/index.php
@@ -4,10 +4,10 @@ declare(strict_types=1);
 session_start();
 
 // กำหนดค่าคงที่สำหรับไดเรกทอรีต่างๆ ในโปรเจค
-const INCLUDES_DIR = __DIR__ . '/../includes';
-const ROUTE_DIR = __DIR__ . '/../routes';
-const TEMPLATES_DIR = __DIR__ . '/../templates';
-const DATABASES_DIR = __DIR__ . '/../databases';
+const INCLUDES_DIR = __DIR__ . '/includes';
+const ROUTE_DIR = __DIR__ . '/routes';
+const TEMPLATES_DIR = __DIR__ . '/templates';
+const DATABASES_DIR = __DIR__ . '/databases';
 
 // รวมไฟล์ที่จำเป็น เข้ามาใช้งานใน index.php
 require_once INCLUDES_DIR . '/router.php';
