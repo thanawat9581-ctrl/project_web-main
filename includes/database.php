fe0e67da5ae11e6193d517bad302ecf463c75697
Affan888 <thnawat9581@gmail.com>
2026-03-14 00:31:10 +0700

    แก้ Nameของ Databases

--- includes/database.php
+++ includes/database.php
@@ -3,9 +3,9 @@ declare(strict_types=1);
 function getConnection(): mysqli
 {
     $hostname = 'localhost';
-    $dbName = 'eventsystem';
-    $username = 'demo';
-    $password = 'abc123';
+    $dbName = 'blabonli_eventsystem';
+    $username = 'blabonli_User1';
+    $password = '@pw112233';
     $conn = new mysqli($hostname, $username, $password, $dbName);
     if ($conn->connect_error) {
         die("Connection failed: " . $conn->connect_error);
