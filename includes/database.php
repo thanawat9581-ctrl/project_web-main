25af8cc6f41af76ad9c566f366ba99ddb4c283d4
Affan888 <thnawat9581@gmail.com>
2026-03-26 00:14:39 +0700

    new DB

--- includes/database.php
+++ includes/database.php
@@ -5,7 +5,7 @@ function getConnection(): mysqli
     $hostname = '*******';
     $dbName = 'blabonli_eventsystem';
     $username = 'blabonli_User1';
-    $password = '@pw112233';
+    $password = '*******';
     $conn = new mysqli($hostname, $username, $password, $dbName);
     if ($conn->connect_error) {
         die("Connection failed: " . $conn->connect_error);
